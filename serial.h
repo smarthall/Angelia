5cd20d7f3444c21923fc4f4710791688161dbe4b
Daniel Hall <daniel@danielhall.me>
2011-08-27 12:26:02 +1000

    typo

--- serial.h
+++ serial.h
@@ -2,5 +2,5 @@
 int getibaud(const struct termios *termios_p);
 int getobaud(const struct termios *termios_p);
 
-#define CHECK_FLAG(bf, f) (((bf) & (f)) == (F))
+#define CHECK_FLAG(bf, f) (((bf) & (f)) == (f))
 
