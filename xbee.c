d44945215de11af9ccc5ab39aeb1445c59803768
Daniel Hall <daniel@danielhall.me>
2012-11-12 08:55:48 +1100

    Buffer handling

--- xbee.c
+++ xbee.c
@@ -1,8 +1,42 @@
 #include "xbee.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 #define HIGH_BYTE(x) ((uint8_t) (x) >> 8)
 #define LOW_BYTE(x) ((uint8_t) (x) & 0x00FF)
 #define COMB_BYTE(h,l) ((uint16_t) ((h << 8) + (l)))
+
+#define BUFSIZE 1024
+
+#define BUFOP_ADD 0
+#define BUFOP_REM 1
+
+int bufferop(int op, int datalen, uint8_t *data) {
+    static uint8_t buffer[BUFSIZE];
+    static uint8_t bufsize = 0;
+
+    switch (op) {
+        case BUFOP_ADD:
+            memcpy(buffer + bufsize, data, datalen);
+            bufsize += datalen;
+            return bufsize;
+        break;
+        case BUFOP_REM:
+            bufsize -= datalen;
+            memmove(buffer, buffer + datalen, bufsize);
+            return bufsize;
+        break;
+    }
+}
+
+int addtobuf(int datalen, uint8_t *data) {
+    return bufferop(BUFOP_ADD, datalen, data);
+}
+
+int remfrombuf(int datalen) {
+    return bufferop(BUFOP_REM, datalen, NULL);
+}
 
 int sendpacket(int fd, uint16_t len, uint8_t *data) {
     uint8_t checksum, start, len_h, len_l;
@@ -24,5 +58,9 @@ int sendpacket(int fd, uint16_t len, uint8_t *data) {
     write(fd, data, len);
     write(fd, &checksum, 1);
 }
+
+int getpacket(int fd, uint8_t *data, uint16_t maxlen) {
+}
+
 
 
