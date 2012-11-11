a115e435cdfeb2b683f3fa2563a742fccb235a43
Daniel Hall <daniel.hall@rea-group.com>
2012-11-12 10:51:11 +1100

    Implemented more buffer management functions

--- xbee.c
+++ xbee.c
@@ -11,8 +11,10 @@
 
 #define BUFOP_ADD 0
 #define BUFOP_REM 1
+#define BUFOP_GET 2
+#define BUFOP_NOOP 3
 
-int bufferop(int op, int datalen, uint8_t *data) {
+int bufferop(int op, int datalen, uint8_t *data, uint8_t **buf) {
     static uint8_t buffer[BUFSIZE];
     static uint8_t bufsize = 0;
 
@@ -27,15 +29,32 @@ int bufferop(int op, int datalen, uint8_t *data) {
             memmove(buffer, buffer + datalen, bufsize);
             return bufsize;
         break;
+        case BUFOP_GET:
+            *buf = buffer;
+            return bufsize;
+        break;
+        case BUFOP_NOOP:
+            return bufsize;
+        break;
     }
 }
 
+uint8_t* getbuf() {
+    uint8_t *buf;
+    bufferop(BUFOP_GET, 0, NULL, &buf);
+    return buf;
+}
+
 int addtobuf(int datalen, uint8_t *data) {
-    return bufferop(BUFOP_ADD, datalen, data);
+    return bufferop(BUFOP_ADD, datalen, data, NULL);
 }
 
 int remfrombuf(int datalen) {
-    return bufferop(BUFOP_REM, datalen, NULL);
+    return bufferop(BUFOP_REM, datalen, NULL, NULL);
+}
+
+int sizebuf() {
+    return bufferop(BUFOP_NOOP, 0, NULL, NULL);
 }
 
 int sendpacket(int fd, uint16_t len, uint8_t *data) {
